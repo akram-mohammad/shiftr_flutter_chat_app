a99cebb59d96c959ed35ab59a23708de77d8fedf
Akram Mohamed <akram.mohamed0@hotmail.com>
2021-06-18 18:07:24 +0200

    mqtt added successfully

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shiftr_app/providers/message_provider.dart';
+import 'package:shiftr_app/ui/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<MessageProvider>(
+      create: (_) => MessageProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MQTTView();
+  }
+}
---
+++ lib/mqtt/mqtt_helper.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:shiftr_app/providers/message_provider.dart';
+
+class MQTTManager {
+  final MessageProvider _currentState;
+  MqttServerClient _client;
+  final String _identifier;
+  final String _host;
+  final String _topic;
+
+  MQTTManager(
+      {@required String host,
+      @required String topic,
+      @required String identifier,
+      @required MessageProvider state})
+      : _identifier = identifier,
+        _host = host,
+        _topic = topic,
+        _currentState = state;
+
+  void initializeMQTTClient() {
+    _client = MqttServerClient('navybee456.cloud.shiftr.io', _identifier);
+    _client.port = 1883;
+    _client.keepAlivePeriod = 20;
+    _client.onDisconnected = onDisconnected;
+    _client.secure = false;
+    _client.logging(on: true);
+
+    _client.onConnected = onConnected;
+
+    final MqttConnectMessage connMess = MqttConnectMessage()
+        .withClientIdentifier(_identifier)
+        .withWillTopic('akram') // If you set this you must set a will message
+        .withWillMessage('My Will message')
+        .startClean() // Non persistent session for testing
+        .withWillQos(MqttQos.atLeastOnce);
+    print('Mosquitto client connecting....');
+    _client.connectionMessage = connMess;
+  }
+
+  void connect() async {
+    assert(_client != null);
+    try {
+      print('Mosquitto start client connecting....');
+      _currentState.setAppConnectionState(MQTTAppConnectionState.connecting);
+      await _client.connect('navybee456', 'amk00000');
+    } on Exception catch (e) {
+      print('Exception : $e');
+      disconnect();
+    }
+  }
+
+  void disconnect() {
+    print('Disconnected');
+    _client.disconnect();
+  }
+
+  void publish(String message) {
+    final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
+    builder.addString(message);
+    _client.publishMessage('akram', MqttQos.exactlyOnce, builder.payload);
+  }
+
+  // _client.onSubscribed = onSubscribed;
+  // void onSubscribed(String topic) {
+  //   print('EXAMPLE::Subscription confirmed for topic $topic');
+  // }
+
+  void onDisconnected() {
+    print('Client disconnection');
+    if (_client.connectionStatus.returnCode ==
+        MqttConnectReturnCode.noneSpecified) {
+      print('OnDisconnected callback is solicited');
+    }
+    _currentState.setAppConnectionState(MQTTAppConnectionState.disconnected);
+  }
+
+  void onConnected() {
+    _currentState.setAppConnectionState(MQTTAppConnectionState.connected);
+    print('Mosquitto client connected....');
+    _client.subscribe('akram', MqttQos.atLeastOnce);
+    _client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final MqttPublishMessage recMess = c[0].payload;
+      final String pt =
+          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+      _currentState.setReceivedText(pt);
+      print('Topic is <${c[0].topic}>, payload is <-- $pt -->');
+    });
+    print('Client connection was Successful');
+  }
+}
